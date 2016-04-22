3652d93c46944abeadbdde05e5ac2557825b2d21
Bdunnigan <bjdunnigan0341@gmail.com>
2016-04-21 17:06:56 -0700

    revised lab6

--- lab6.sh
+++ lab6.sh
@@ -2,5 +2,7 @@
 
 PARSED_XML=$(grep .mp[3,g] /root/medialab/media.xml)
 PARSED_XML=$(echo "${PARSED_XML}" | awk 'BEGIN { FS = ">" } { print$2 }')
-PARSED_XML=$(echo "${PARSED_XML}" | awk 'BEGIN { FS = "<" } { print$1 }')"\n"
-echo ${PARSED_XML}
+PARSED_XML=$(echo "${PARSED_XML}" | awk 'BEGIN { FS = "<" } { print$0 } END { OFS = "\n" }')
+echo ${PARSED_XML} > /root/parsed_xml.txt
+
+
